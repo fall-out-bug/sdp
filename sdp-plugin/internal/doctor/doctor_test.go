fda60076a54e9431255bd18bc838e934cbe00ca0
Andrei <a_v_zhukov@outlook.com>
2026-02-06 18:56:47 +0300

    test(sdp-7py): increase ui package coverage 70.9% → 91.8%
    - TestInstallCompletion_Bash: Tests bash completion installation
    - TestInstallCompletion_Zsh: Tests zsh completion installation
    - TestInstallCompletion_Fish: Tests fish completion installation
    - TestInstallCompletion_NoHome: Tests error when HOME not set
    - TestInstallCompletion_UnsupportedShell: Tests error for invalid shell
    - TestInstallCompletion_CreatesDirectory: Tests directory creation
    
    Coverage increased from 70.9% to 91.8% (20.9% improvement, 114% of 80% target).
    InstallCompletion function now at 93.3% coverage (previously 0%).

--- sdp-plugin/internal/doctor/doctor_test.go
+++ sdp-plugin/internal/doctor/doctor_test.go
@@ -328,7 +328,7 @@ func TestFindRecentWorkstreamsForDrift_EmptyDirectories(t *testing.T) {
 func contains(s, substr string) bool {
 	return len(s) >= len(substr) && (s == substr || len(s) > len(substr) &&
 		(s[:len(substr)] == substr || s[len(s)-len(substr):] == substr ||
-	 indexOf(s, substr) >= 0))
+			indexOf(s, substr) >= 0))
 }
 
 func indexOf(s, substr string) int {
