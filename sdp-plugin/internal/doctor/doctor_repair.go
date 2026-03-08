b27699362ca607a34f4edb7e9fb2e1e2e63ad5f9
Andrey Zhukov <a_v_zhukov@outlook.com>
2026-03-08 16:15:43 +0300

    refactor(doctor): use 0o file modes

--- sdp-plugin/internal/doctor/doctor_repair.go
+++ sdp-plugin/internal/doctor/doctor_repair.go
@@ -41,7 +41,7 @@ func repairClaudeDirs() RepairAction {
 
 	for _, dir := range requiredDirs {
 		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err := os.MkdirAll(dir, 0755); err != nil {
+			if err := os.MkdirAll(dir, 0o755); err != nil {
 				failed = append(failed, dir)
 			} else {
 				created = append(created, dir)
@@ -92,7 +92,7 @@ func repairSDPDirs() RepairAction {
 
 	for _, dir := range requiredDirs {
 		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err := os.MkdirAll(dir, 0755); err != nil {
+			if err := os.MkdirAll(dir, 0o755); err != nil {
 				failed = append(failed, dir)
 			} else {
 				created = append(created, dir)
